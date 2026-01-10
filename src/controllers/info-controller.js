6e8230fbdfc88683f1a244df3df6b3f8bc902c58
suvm-sharma <suvmsharma121@gmail.com>
2026-01-10 21:55:16 +0530

    feat: initialize base Node.js project template with essential configurations
    - Added package.json with dependencies and scripts for development
    - Created configuration files for server and logger
    - Implemented a basic info controller with a health check endpoint
    - Set up routing structure for API versioning
    - Established main entry point for the application

---
+++ src/controllers/info-controller.js
@@ -0,0 +1,14 @@
+const { StatusCodes } = require("http-status-codes");
+
+const info = function (_req, res) {
+    return res.status(StatusCodes.OK).json({
+        success: true,
+        message: "API is live",
+        error: {},
+        data: {},
+    });
+};
+
+module.exports = {
+    info,
+};
